b10311fff35cc659e293a0919675b8ac3d3dcf63
Utsav Singh <utsavgwa@gmail.com>
2022-10-28 04:10:54 +0530

    created bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -49,17 +49,6 @@ class _GreetState extends State<Greet> {
   }
 }
 
-class HelloWorld extends StatelessWidget {
-  //it a constructor, recieves a key, and passing it to the super class, because it is mandatory
-  const HelloWorld({super.key});
-
-  @override // this is an annotation, it is used to override the method of the parent class
-  Widget build(BuildContext context) {
-    //Widget here is a return type
-    return const Text("Hello World");
-  }
-}
-
 class MyApp extends StatelessWidget {
   //it a constructor, recieves a key, and passing it to the super class, because it is mandatory
   const MyApp({super.key});
@@ -67,8 +56,9 @@ class MyApp extends StatelessWidget {
   // @override-is an annotation, it is used to override the method of the parent class
   @override
   Widget build(BuildContext context) {
-    //title ofthe material app is used in OS
+    //Widget here is a return type
     return MaterialApp(
+      //title ofthe material app is used in OS
       title: 'Roasted Beans',
       theme: ThemeData(
         //here we can declare global themes, colors declarations that can be used anywhere in widget tree
@@ -91,15 +81,61 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//this is my state, private class
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    Widget currentWidgetPage = const Text("dummmy");
+
+    switch (selectedIndex) {
+      case 0:
+        currentWidgetPage = const Text("Menu Page");
+        break;
+      case 1:
+        currentWidgetPage = const OffersPage();
+        break;
+      case 2:
+        currentWidgetPage = const Text("Orders Page");
+        break;
+      default:
+    }
+
+    //scaffold is an constructor, can't put switch or if statement in one arguement of scaffold
     return Scaffold(
+      //here appBar is arguement of Scaffold
       appBar: AppBar(
         centerTitle: true,
         title: Image.asset("images/logo.png"),
       ),
-      body: const OffersPage(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (newIndex) {
+            // setState takes function as an arguement
+            setState(() {
+              selectedIndex = newIndex; //changin the state
+            });
+          },
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.yellow.shade400,
+          unselectedItemColor: Colors.brown.shade50,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Menu",
+              icon: Icon(Icons.coffee),
+            ),
+            BottomNavigationBarItem(
+              label: "Offers",
+              icon: Icon(Icons.local_offer_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: "Orders",
+              icon: Icon(Icons.shopping_cart_checkout_rounded),
+            ),
+          ]),
+      //body is also arguement
+      body: currentWidgetPage,
     );
   }
 }
