0bcf2de1fd44347ad7e43e6207ad82ff2351a37e
Utsav Singh <utsavgwa@gmail.com>
2022-10-28 05:51:20 +0530

    product card added

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../datamodel.dart';
+
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var p = Product(id: 1, name: "Dummy", price: 2.3, image: "");
+    return ProductItem(product: p);
+  }
+}
+
+//this renders one product on the screen
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(6),
+      child: Card(
+          elevation: 4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset("images/black_coffee.png"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  product.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("\$${product.price}"),
+              ),
+            ],
+          )),
+    );
   }
 }
